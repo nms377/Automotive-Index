b54962e71e9031539ee7e7a72e5dea19861dc304
nms377 <nms377@nyu.edu>
2017-01-19 21:25:09 -1000

    Completed query number 3, but I had to drop my car_models tables first because I added an extra table, which threw off my row count.

--- indexing.sql
+++ indexing.sql
@@ -32,3 +32,7 @@ SELECT DISTINCT model_title
 FROM car_models
 WHERE make_code = 'NISSAN'
 AND model_code = 'GT-R';
+
+SELECT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
