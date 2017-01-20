0757901497cdf1202c24b50d740bc7676be915a4
nms377 <nms377@nyu.edu>
2017-01-19 20:31:50 -1000

    Created the user, created the database. Had errors trying to run the file in the terminal.

--- indexing.sql
+++ indexing.sql
@@ -1,3 +1,11 @@
+\c nicolesandry;
+
+DROP USER "indexed_cars_user";
+
 CREATE USER "indexed_cars_user";
 
-CREATE DATABASE "indexed_cars" OWNER indexed_cars_user;
+DROP DATABASE IF EXISTS "indexed_cars";
+
+CREATE DATABASE "indexed_cars" OWNER "indexed_cars_user";
+
+\c indexed_cars indexed_cars_user;
