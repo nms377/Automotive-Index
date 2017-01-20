9e02a2c8612a368e025889e95bdf207eefcbe487
nms377 <nms377@nyu.edu>
2017-01-19 20:37:10 -1000

    Ran the scripts/car_models.sql and scripts/car_model_data.sql ten times. Used SELECT * FROM car_models to see the table in the terminal.

--- indexing.sql
+++ indexing.sql
@@ -9,3 +9,15 @@ DROP DATABASE IF EXISTS "indexed_cars";
 CREATE DATABASE "indexed_cars" OWNER "indexed_cars_user";
 
 \c indexed_cars indexed_cars_user;
+
+\i scripts/car_models.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
