8d0155585cea4aaf33da5f62fab85ff17419e873
nms377 <nms377@nyu.edu>
2017-01-20 00:38:12 -1000

    Transferred my recorded index statements from indexing.sql to this file.

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,14 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+--CREATE INDEXES
+CREATE INDEX make_code
+	ON car_models (make_code);	
+
+CREATE INDEX model_code
+	ON car_models (model_code);
+
+CREATE INDEX year
+	ON car_models (year);
